3658f08f7d4cccfa600b82f353aa45e62e5e1a11
Rory <Rory.Martin@equitymethods.com>
2022-06-27 14:14:03 -0700

    finished basic worksheet creation, functions need more modularity

--- driver.py
+++ driver.py
@@ -2,11 +2,6 @@ import numpy as np
 import generate
 
 def main():
-    for problem in generate.two_step_whole():
-        generate.show_steps_two_step(problem)
-        # print(i, ": ", problem["a_sign"], problem["a"], problem["solve_for"], problem["b_sign"], problem["b"], "=", problem["c_sign"], problem["c"], sep="")
-        # print(problem["solve_for"], "=", problem["solution"])
-
-
+    generate.worksheet(10)
 if __name__ == "__main__":
     main()
--- generate.py
+++ generate.py
@@ -1,21 +1,26 @@
 import random
-from string import ascii_lowercase
+from string import ascii_lowercase, printable
+from fpdf import FPDF, HTMLMixin
 
-def two_step_equation():
+#Generates two step equation problems and stores it in a dictionary
+def two_step_equation(high=100.0):
     #ax +- b = c
+    safe_lowercase = ascii_lowercase.replace("l", "").replace("o", "").replace("i", "").replace("e", "")
     problem = {
-        "solve_for": random.choice(ascii_lowercase),
-        "a": random.randrange(1.0, 100.0, 1),
+        "solve_for": random.choice(safe_lowercase),
+        "a": random.randrange(1.0, high, 1),
         "a_sign": random.choice(["", "-"]),
         "a_mult": 1,
-        "b": random.randrange(0.0, 100.0, 1),
+        "b": random.randrange(0.0, high, 1),
         "b_sign": random.choice(["+", "-"]),
         "b_mult": 1,
-        "c": random.randrange(0.0, 100.0, 1),
+        "c": random.randrange(0.0, high, 1),
         "c_sign": random.choice(["", "-"]),
         "c_mult": 1,
         "solution": 0.0,
+        "printable": "",
     }
+    problem["printable"] = str(problem["a_sign"]) + str(problem["a"]) + problem["solve_for"] + " " + str(problem["b_sign"]) + str(problem["b"]) + " = " + str(problem["c_sign"]) + str(problem["c"])
     if(problem["a_sign"] == "-"):
         problem["a_mult"] = -1
     if(problem["b_sign"] == "-"):
@@ -27,6 +32,7 @@ def two_step_equation():
    
     return problem
 
+#Generates two step problems and then verifies they are whole number solutions, returns a list of dictionaries
 def two_step_whole():
     list = []
     while len(list) < 100:
@@ -35,6 +41,7 @@ def two_step_whole():
            list.append(problem)
     return list
 
+#Generates two step problems and then verifies they are not whole number solutions, returns a list of dictionaries
 def two_step_frac():
     list = []
     while len(list) < 100:
@@ -43,9 +50,25 @@ def two_step_frac():
            list.append(problem)
     return list
 
+#Demonstrates the steps to complete a two step equation (in more than two steps...)
 def show_steps_two_step(problem):
     print("Base problem: ", problem["a_sign"], problem["a"], problem["solve_for"], problem["b_sign"], problem["b"], "=", problem["c_sign"], problem["c"], sep="")
     print("Step 1 Move second term over: ", problem["a_sign"], problem["a"], problem["solve_for"], "=", problem["c_sign"], problem["c"], "-(", ("" if problem["b_sign"] == "+" else "-"), problem["b"], ")", sep="")
     print("Step 2 Combine right side terms: ", problem["a_sign"], problem["a"], problem["solve_for"], "=", (problem["c_mult"] * problem["c"]) + (problem["b_mult"]**2 * problem["b"]), sep="")
     print("Step 3 Divide both sides by coefficient: ", problem["solve_for"], "=", (problem["c_mult"] * problem["c"]) + (problem["b_mult"]**2 * problem["b"]), "/", problem["a_sign"], problem["a"], sep="")
     print("Step 4 Simplify: ", problem["solve_for"], "=", problem["solution"], sep="")
+
+def worksheet(num_problems=5, problem_type="two_step"):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Times", "", size=15)
+    line_height = pdf.font_size * 2.5
+    col_width = pdf.epw / 2
+    pdf.cell(pdf.epw, line_height, txt="Two Step Problems", ln=1, align='L')
+    list = [two_step_equation() for _ in range(num_problems)]
+    data = ()
+    for i in range(0, len(list), 2):
+        pdf.multi_cell(col_width, line_height, str(i+1) + ") " + list[i]["printable"], border=0, new_x="RIGHT", new_y="TOP", max_line_height=pdf.font_size)
+        pdf.multi_cell(col_width, line_height, str(i+2) + ") " + list[i+1]["printable"], border=0, new_x="RIGHT", new_y="TOP", max_line_height=pdf.font_size)
+        pdf.ln(line_height*2.5)
+    pdf.output("GFG.pdf")
